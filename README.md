f5876d46b6367dba96a6fae0d6bda2cf03645ce7
Justherozen <2271493535@qq.com>
2020-07-15 02:46:58 +0800

    714xrx提交

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # bytedance_minitiktok
 the minitiktok project of 2020 summer camp
+
+
+
+714修改：
+
+add splash 
+
+add 消息界面
+
+add 其他导航
+
+add app icon
+
+修改了视频播放页面的ui
+
---
+++ app/src/main/java/com/example/minitiktok/bean/PostVideoResponse.java
@@ -0,0 +1,16 @@
+package com.example.minitiktok.bean;
+
+
+import com.google.gson.annotations.SerializedName;
+
+public class PostVideoResponse {
+
+    @SerializedName("success") private boolean success;
+    @SerializedName("item") private Feed item;
+    public boolean isSuccess(){
+        return success;
+    }
+    public Feed getItem(){
+        return item;
+    }
+}
